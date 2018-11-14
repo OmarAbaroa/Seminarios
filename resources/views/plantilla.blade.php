0500adeef0d78391945d0fc58ff5cbef73512fa8
OmarAbaroa <alvaro1295@hotmail.com>
2018-11-14 10:43:28 -0600

    Descarga después de 0 segundos

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{csrf_token()}}">
         @if(Session::has('download.in.the.next.request'))
-             <meta http-equiv="refresh" content="5;url={{ Session::get('download.in.the.next.request') }}">
+             <meta http-equiv="refresh" content="0;url={{ Session::get('download.in.the.next.request') }}">
         @endif
         <title>COSIE - SICOSE | @yield('titulo')</title>
 
